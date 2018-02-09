c7f872948bbea3e31eefe53acd9c357c4a8b8e83
Unknown <david.lafreniere@live.ca>
2018-02-09 09:27:09 -0500

    Icons now use project ressources instead of files in Images folder

--- PrjEq01/PrjEq01_CommonForm/PrjEq01_CommonForm/FormToolsStandard.Designer.cs
+++ PrjEq01/PrjEq01_CommonForm/PrjEq01_CommonForm/FormToolsStandard.Designer.cs
@@ -28,24 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Button_Function = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // Button_Function
-            // 
-            this.Button_Function.Location = new System.Drawing.Point(110, 72);
-            this.Button_Function.Name = "Button_Function";
-            this.Button_Function.Size = new System.Drawing.Size(229, 136);
-            this.Button_Function.TabIndex = 0;
-            this.Button_Function.Text = "Button_Function";
-            this.Button_Function.UseVisualStyleBackColor = true;
             // 
             // FormToolsStandard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(729, 385);
-            this.Controls.Add(this.Button_Function);
             this.Name = "FormToolsStandard";
             this.Text = "FormToolsStandard";
             this.ResumeLayout(false);
@@ -53,7 +42,5 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button Button_Function;
     }
 }
