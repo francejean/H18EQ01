5f1dca36edff81c810c38beb477ade36aa1cab35
Unknown <david.lafreniere@live.ca>
2018-02-19 10:41:58 -0500

    Tab arrive user control placed in tab, application name

--- PrjEq01/PrjEq01_Application/PrjEq01_Application/FormApplication.Designer.cs
+++ PrjEq01/PrjEq01_Application/PrjEq01_Application/FormApplication.Designer.cs
@@ -28,12 +28,37 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+            this.tab_Arrive1 = new PrjEq01_Application.Tabs.Tab_Arrive();
+            this.Tab_Départs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Tab_Départs
+            // 
+            this.Tab_Départs.Controls.Add(this.tab_Arrive1);
+            // 
+            // tab_Arrive1
+            // 
+            this.tab_Arrive1.Location = new System.Drawing.Point(3, 3);
+            this.tab_Arrive1.Name = "tab_Arrive1";
+            this.tab_Arrive1.Size = new System.Drawing.Size(1291, 526);
+            this.tab_Arrive1.TabIndex = 0;
+            // 
+            // FormApplication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1329, 603);
+            this.Name = "FormApplication";
+            this.Text = "Hotel Simulator 2018";
+            this.Tab_Départs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private Tabs.Tab_Arrive tab_Arrive1;
+    }
 }
 
