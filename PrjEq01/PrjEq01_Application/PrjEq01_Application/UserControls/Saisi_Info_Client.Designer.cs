b0cfbf0b57fa03de10d713059a760130a0fefd50
Unknown <david.lafreniere@live.ca>
2018-02-26 08:57:42 -0500

    Set anchor on controls

--- PrjEq01/PrjEq01_Application/PrjEq01_Application/UserControls/Saisi_Info_Client.Designer.cs
+++ PrjEq01/PrjEq01_Application/PrjEq01_Application/UserControls/Saisi_Info_Client.Designer.cs
@@ -71,8 +71,10 @@
             this.boxClient.Controls.Add(this.label2);
             this.boxClient.Controls.Add(this.label1);
             this.boxClient.Location = new System.Drawing.Point(0, 0);
+            this.boxClient.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.boxClient.Name = "boxClient";
-            this.boxClient.Size = new System.Drawing.Size(365, 190);
+            this.boxClient.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.boxClient.Size = new System.Drawing.Size(487, 234);
             this.boxClient.TabIndex = 1;
             this.boxClient.TabStop = false;
             this.boxClient.Text = "Informations du client";
@@ -82,146 +84,158 @@
             this.button_List1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button_List1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_List1.BackgroundImage")));
             this.button_List1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button_List1.Location = new System.Drawing.Point(339, 10);
-            this.button_List1.Margin = new System.Windows.Forms.Padding(2);
+            this.button_List1.Location = new System.Drawing.Point(452, 12);
+            this.button_List1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button_List1.Name = "button_List1";
-            this.button_List1.Size = new System.Drawing.Size(20, 20);
+            this.button_List1.Size = new System.Drawing.Size(27, 25);
             this.button_List1.TabIndex = 1;
             this.button_List1.UseVisualStyleBackColor = true;
             // 
             // noCarteText
             // 
-            this.noCarteText.Location = new System.Drawing.Point(259, 113);
+            this.noCarteText.Location = new System.Drawing.Point(345, 139);
+            this.noCarteText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.noCarteText.Name = "noCarteText";
-            this.noCarteText.Size = new System.Drawing.Size(100, 20);
+            this.noCarteText.Size = new System.Drawing.Size(132, 22);
             this.noCarteText.TabIndex = 13;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(201, 116);
+            this.label8.Location = new System.Drawing.Point(268, 143);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(52, 13);
+            this.label8.Size = new System.Drawing.Size(68, 17);
             this.label8.TabIndex = 12;
             this.label8.Text = "No. Carte";
             // 
             // nomClientText
             // 
-            this.nomClientText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.nomClientText.Location = new System.Drawing.Point(259, 35);
+            this.nomClientText.Location = new System.Drawing.Point(345, 43);
+            this.nomClientText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.nomClientText.Name = "nomClientText";
-            this.nomClientText.Size = new System.Drawing.Size(100, 20);
+            this.nomClientText.Size = new System.Drawing.Size(132, 22);
             this.nomClientText.TabIndex = 11;
             // 
             // label7
             // 
-            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(201, 38);
+            this.label7.Location = new System.Drawing.Point(268, 47);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.Size = new System.Drawing.Size(37, 17);
             this.label7.TabIndex = 10;
             this.label7.Text = "Nom";
             // 
             // typeCarteText
             // 
-            this.typeCarteText.Location = new System.Drawing.Point(87, 113);
+            this.typeCarteText.Location = new System.Drawing.Point(116, 139);
+            this.typeCarteText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.typeCarteText.Name = "typeCarteText";
-            this.typeCarteText.Size = new System.Drawing.Size(100, 20);
+            this.typeCarteText.Size = new System.Drawing.Size(132, 22);
             this.typeCarteText.TabIndex = 9;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(7, 116);
+            this.label6.Location = new System.Drawing.Point(9, 143);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(74, 13);
+            this.label6.Size = new System.Drawing.Size(98, 17);
             this.label6.TabIndex = 8;
             this.label6.Text = "Type de Carte";
             // 
             // expirationText
             // 
-            this.expirationText.Location = new System.Drawing.Point(87, 139);
+            this.expirationText.Location = new System.Drawing.Point(116, 171);
+            this.expirationText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.expirationText.Name = "expirationText";
-            this.expirationText.Size = new System.Drawing.Size(100, 20);
+            this.expirationText.Size = new System.Drawing.Size(132, 22);
             this.expirationText.TabIndex = 7;
             // 
             // telephoneText
             // 
-            this.telephoneText.Location = new System.Drawing.Point(87, 87);
+            this.telephoneText.Location = new System.Drawing.Point(116, 107);
+            this.telephoneText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.telephoneText.Name = "telephoneText";
-            this.telephoneText.Size = new System.Drawing.Size(100, 20);
+            this.telephoneText.Size = new System.Drawing.Size(132, 22);
             this.telephoneText.TabIndex = 6;
             // 
             // adresseText
             // 
-            this.adresseText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.adresseText.Location = new System.Drawing.Point(87, 61);
+            this.adresseText.Location = new System.Drawing.Point(116, 75);
+            this.adresseText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.adresseText.Name = "adresseText";
-            this.adresseText.Size = new System.Drawing.Size(272, 20);
+            this.adresseText.Size = new System.Drawing.Size(361, 22);
             this.adresseText.TabIndex = 5;
             // 
             // noClienText
             // 
-            this.noClienText.Location = new System.Drawing.Point(87, 35);
+            this.noClienText.Location = new System.Drawing.Point(116, 43);
+            this.noClienText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.noClienText.Name = "noClienText";
-            this.noClienText.Size = new System.Drawing.Size(100, 20);
+            this.noClienText.Size = new System.Drawing.Size(132, 22);
             this.noClienText.TabIndex = 1;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(7, 142);
+            this.label5.Location = new System.Drawing.Point(9, 175);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.Size = new System.Drawing.Size(70, 17);
             this.label5.TabIndex = 4;
             this.label5.Text = "Expiration";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 68);
+            this.label4.Location = new System.Drawing.Point(8, 84);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(7, 13);
+            this.label4.Size = new System.Drawing.Size(8, 17);
             this.label4.TabIndex = 3;
             this.label4.Text = "\t\t\t\r\n";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(7, 90);
+            this.label3.Location = new System.Drawing.Point(9, 111);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.Size = new System.Drawing.Size(76, 17);
             this.label3.TabIndex = 2;
             this.label3.Text = "Téléphone";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 64);
+            this.label2.Location = new System.Drawing.Point(9, 79);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.Size = new System.Drawing.Size(60, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Adresse";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 38);
+            this.label1.Location = new System.Drawing.Point(9, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.Size = new System.Drawing.Size(63, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Client no";
             // 
             // Saisi_Info_Client
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.boxClient);
-            this.MinimumSize = new System.Drawing.Size(365, 190);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(487, 234);
             this.Name = "Saisi_Info_Client";
-            this.Size = new System.Drawing.Size(365, 190);
+            this.Size = new System.Drawing.Size(487, 234);
             this.boxClient.ResumeLayout(false);
             this.boxClient.PerformLayout();
             this.ResumeLayout(false);
