10dcd1196f31d854e0c6c1094afc1524c4239427
Unknown <paul-david_traore@hotmail.com>
2018-03-16 11:09:11 -0400

    UC_Chambre readOnly = true

--- PrjEq01/PrjEq01_Application/PrjEq01_Application/UserControls/Info_Reservation/IR_Depart.Designer.cs
+++ PrjEq01/PrjEq01_Application/PrjEq01_Application/UserControls/Info_Reservation/IR_Depart.Designer.cs
@@ -28,171 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IR_Depart));
-            this.gb_reserv = new System.Windows.Forms.GroupBox();
-            this.DTP_Fin = new System.Windows.Forms.DateTimePicker();
-            this.DTP_Debut = new System.Windows.Forms.DateTimePicker();
-            this.DTP_Reserv = new System.Windows.Forms.DateTimePicker();
-            this.bt_list = new PrjEq01_CommonForm.Controls.Button_List();
-            this.tb_noReserv = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
+            this.tb_confirmerPar = new System.Windows.Forms.TextBox();
+            this.lb_confirmerPar = new System.Windows.Forms.Label();
             this.gb_reserv.SuspendLayout();
             this.SuspendLayout();
             // 
             // gb_reserv
             // 
-            this.gb_reserv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.gb_reserv.Controls.Add(this.tb_confirmerPar);
+            this.gb_reserv.Controls.Add(this.lb_confirmerPar);
+            this.gb_reserv.Controls.SetChildIndex(this.tb_noReserv, 0);
+            this.gb_reserv.Controls.SetChildIndex(this.DTP_Reserv, 0);
+            this.gb_reserv.Controls.SetChildIndex(this.DTP_Debut, 0);
+            this.gb_reserv.Controls.SetChildIndex(this.DTP_Fin, 0);
+            this.gb_reserv.Controls.SetChildIndex(this.lb_confirmerPar, 0);
+            this.gb_reserv.Controls.SetChildIndex(this.tb_confirmerPar, 0);
+            // 
+            // tb_confirmerPar
+            // 
+            this.tb_confirmerPar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.gb_reserv.Controls.Add(this.textBox1);
-            this.gb_reserv.Controls.Add(this.label5);
-            this.gb_reserv.Controls.Add(this.DTP_Fin);
-            this.gb_reserv.Controls.Add(this.DTP_Debut);
-            this.gb_reserv.Controls.Add(this.DTP_Reserv);
-            this.gb_reserv.Controls.Add(this.bt_list);
-            this.gb_reserv.Controls.Add(this.tb_noReserv);
-            this.gb_reserv.Controls.Add(this.label4);
-            this.gb_reserv.Controls.Add(this.label1);
-            this.gb_reserv.Controls.Add(this.label2);
-            this.gb_reserv.Controls.Add(this.label3);
-            this.gb_reserv.Location = new System.Drawing.Point(0, 0);
-            this.gb_reserv.Margin = new System.Windows.Forms.Padding(4);
-            this.gb_reserv.MinimumSize = new System.Drawing.Size(259, 209);
-            this.gb_reserv.Name = "gb_reserv";
-            this.gb_reserv.Padding = new System.Windows.Forms.Padding(4);
-            this.gb_reserv.Size = new System.Drawing.Size(267, 234);
-            this.gb_reserv.TabIndex = 1;
-            this.gb_reserv.TabStop = false;
-            this.gb_reserv.Text = "Information sur la réservation";
+            this.tb_confirmerPar.Location = new System.Drawing.Point(129, 169);
+            this.tb_confirmerPar.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_confirmerPar.Name = "tb_confirmerPar";
+            this.tb_confirmerPar.Size = new System.Drawing.Size(128, 22);
+            this.tb_confirmerPar.TabIndex = 27;
             // 
-            // DTP_Fin
+            // lb_confirmerPar
             // 
-            this.DTP_Fin.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.DTP_Fin.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.DTP_Fin.Location = new System.Drawing.Point(129, 139);
-            this.DTP_Fin.Margin = new System.Windows.Forms.Padding(4);
-            this.DTP_Fin.Name = "DTP_Fin";
-            this.DTP_Fin.Size = new System.Drawing.Size(128, 22);
-            this.DTP_Fin.TabIndex = 21;
-            // 
-            // DTP_Debut
-            // 
-            this.DTP_Debut.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.DTP_Debut.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.DTP_Debut.Location = new System.Drawing.Point(129, 107);
-            this.DTP_Debut.Margin = new System.Windows.Forms.Padding(4);
-            this.DTP_Debut.Name = "DTP_Debut";
-            this.DTP_Debut.Size = new System.Drawing.Size(128, 22);
-            this.DTP_Debut.TabIndex = 20;
-            // 
-            // DTP_Reserv
-            // 
-            this.DTP_Reserv.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.DTP_Reserv.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.DTP_Reserv.Location = new System.Drawing.Point(129, 75);
-            this.DTP_Reserv.Margin = new System.Windows.Forms.Padding(4);
-            this.DTP_Reserv.Name = "DTP_Reserv";
-            this.DTP_Reserv.Size = new System.Drawing.Size(128, 22);
-            this.DTP_Reserv.TabIndex = 19;
-            // 
-            // bt_list
-            // 
-            this.bt_list.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.bt_list.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bt_list.BackgroundImage")));
-            this.bt_list.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.bt_list.Location = new System.Drawing.Point(232, 12);
-            this.bt_list.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.bt_list.Name = "bt_list";
-            this.bt_list.Size = new System.Drawing.Size(27, 25);
-            this.bt_list.TabIndex = 1;
-            this.bt_list.UseVisualStyleBackColor = true;
-            // 
-            // tb_noReserv
-            // 
-            this.tb_noReserv.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tb_noReserv.Location = new System.Drawing.Point(129, 43);
-            this.tb_noReserv.Margin = new System.Windows.Forms.Padding(4);
-            this.tb_noReserv.Name = "tb_noReserv";
-            this.tb_noReserv.Size = new System.Drawing.Size(128, 22);
-            this.tb_noReserv.TabIndex = 18;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(9, 143);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(77, 17);
-            this.label4.TabIndex = 13;
-            this.label4.Text = "Date de fin";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 47);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 17);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "No. Réservation";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 79);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 17);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Réservé le";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 111);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(98, 17);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Date de début";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(129, 169);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(128, 22);
-            this.textBox1.TabIndex = 25;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(9, 172);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(89, 17);
-            this.label5.TabIndex = 24;
-            this.label5.Text = "Confirmé par";
+            this.lb_confirmerPar.AutoSize = true;
+            this.lb_confirmerPar.Location = new System.Drawing.Point(9, 172);
+            this.lb_confirmerPar.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_confirmerPar.Name = "lb_confirmerPar";
+            this.lb_confirmerPar.Size = new System.Drawing.Size(89, 17);
+            this.lb_confirmerPar.TabIndex = 26;
+            this.lb_confirmerPar.Text = "Confirmé par";
             // 
             // IR_Depart
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.gb_reserv);
-            this.MinimumSize = new System.Drawing.Size(267, 234);
             this.Name = "IR_Depart";
-            this.Size = new System.Drawing.Size(267, 234);
             this.gb_reserv.ResumeLayout(false);
             this.gb_reserv.PerformLayout();
             this.ResumeLayout(false);
@@ -201,17 +77,7 @@
 
         #endregion
 
-        public System.Windows.Forms.GroupBox gb_reserv;
-        public System.Windows.Forms.DateTimePicker DTP_Fin;
-        public System.Windows.Forms.DateTimePicker DTP_Debut;
-        public System.Windows.Forms.DateTimePicker DTP_Reserv;
-        private PrjEq01_CommonForm.Controls.Button_List bt_list;
-        public System.Windows.Forms.TextBox tb_noReserv;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        public System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label5;
+        public System.Windows.Forms.TextBox tb_confirmerPar;
+        private System.Windows.Forms.Label lb_confirmerPar;
     }
 }
