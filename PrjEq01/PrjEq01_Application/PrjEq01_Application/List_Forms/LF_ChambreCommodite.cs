f6be77bb3139b030c95b7320fb118fececefc73d
Unknown <paul-david_traore@hotmail.com>
2018-03-19 02:56:46 -0400

    Ajout LF_ChambreCodeType, LF_ChambreCodeLoc et LF_ChambreCommodite

---
+++ PrjEq01/PrjEq01_Application/PrjEq01_Application/List_Forms/LF_ChambreCommodite.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrjEq01_Application.List_Forms
+{
+    public partial class LF_ChambreCommodite : PrjEq01_Application.List_Forms.LF_Base
+    {
+        public LF_ChambreCommodite()
+        {
+            InitializeComponent();
+        }
+
+        public PrjEq01_Application.UserControls.Inherit_DGV Dgv_main
+        {
+            get
+            {
+                return this.dgv_main;
+            }
+            set
+            {
+                dgv_main = value;
+            }
+        }
+    }
+}
--- PrjEq01/PrjEq01_Application/PrjEq01_Application/Properties/Settings.Designer.cs
+++ PrjEq01/PrjEq01_Application/PrjEq01_Application/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace PrjEq01_Application.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,8 @@ namespace PrjEq01_Application.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\David\\Documents\\Loc" +
-            "al Database.mdf\";Integrated Security=True;Connect Timeout=30")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=h18equipe1;Integrated Security" +
+            "=True")]
         public string h18equipe1ConnectionString {
             get {
                 return ((string)(this["h18equipe1ConnectionString"]));
