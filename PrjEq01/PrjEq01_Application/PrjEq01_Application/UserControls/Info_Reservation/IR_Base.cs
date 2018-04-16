d01163bc51cfb413a489651d594a7bd90c6adad1
djeezuss <olivierleb25@gmail.com>
2018-04-16 10:56:21 -0400

    Reformated IR_Base + Added IR_Reserv

--- PrjEq01/PrjEq01_Application/PrjEq01_Application/UserControls/Info_Reservation/IR_Base.cs
+++ PrjEq01/PrjEq01_Application/PrjEq01_Application/UserControls/Info_Reservation/IR_Base.cs
@@ -11,32 +11,32 @@ using PrjEq01_CommonForm;
 
 namespace PrjEq01_Application.UserControls
 {
-	public partial class IR_Base: UserControl, IInfoBox
+	public partial class IR_Base : UserControl, IInfoBox
 	{
-        protected BindingSource BS;
+		protected BindingSource BS;
 
-        private ItemSelectedDeleg reservSelected;
-        public ItemSelectedDeleg ReservSelected { get => reservSelected; set => reservSelected = value; }
+		private ItemSelectedDeleg reservSelected;
+		public ItemSelectedDeleg ReservSelected { get => reservSelected; set => reservSelected = value; }
 
-        public IR_Base()
+		public IR_Base()
 		{
 			InitializeComponent();
-            TA_RESERVATION.Fill(ds_master.RESERVATION);
+			TA_RESERVATION.Fill(ds_master.RESERVATION);
 		}
 
-    public void setBS(BindingSource BS)
-    {
-        this.BS = BS;
-    }
+		public void setBS(BindingSource BS)
+		{
+			this.BS = BS;
+		}
 
-    protected virtual void bt_list_Click(object sender, EventArgs e)
-    {
-        reservSelected();
-    }
+		protected virtual void bt_list_Click(object sender, EventArgs e)
+		{
+			reservSelected();
+		}
 
-    public void SetReadOnly(States state)
-    {
-      bool readOnly	= States.CONSULT == state;
+		public void SetReadOnly(States state)
+		{
+			bool readOnly = States.CONSULT == state;
 
 			// Different for Reserv and Arrive
 			bt_list.Enabled = (state == States.ADD);
@@ -52,10 +52,11 @@ namespace PrjEq01_Application.UserControls
 
 		public virtual void WipeInformation()
 		{
-      foreach (Control ctrl in gb_reserv.Controls)
-      {
-        if (ctrl is TextBox || ctrl is DateTimePicker)
-          ctrl.ResetText();
-    }
-  }
+			foreach (Control ctrl in gb_reserv.Controls)
+			{
+				if (ctrl is TextBox || ctrl is DateTimePicker)
+					ctrl.ResetText();
+			}
+		}
+	}
 }
